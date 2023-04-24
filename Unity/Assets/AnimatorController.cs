edb154a71f88be491c7cd5199a198ae265adfa0d
JanPelicon <jnpelicon@gmail.com>
2023-04-24 12:37:17 +0200

    Some work on prototype

--- Unity/Assets/AnimatorController.cs
+++ Unity/Assets/AnimatorController.cs
@@ -6,26 +6,38 @@ using UnityEngine.UI;
 
 public class AnimatorController : MonoBehaviour
 {
+
     bool playing = true;
     float speed = 1.0f;
 
-    Slider m_Slider;
-    Animator m_Animator;
     Button b_Play;
     Button b_Stop;
+    Button b_Reset;
+    Slider m_Slider;
+
+    Animator m_Animator;
 
     void Start()
     {
-        m_Animator = GameObject.Find("Cube").GetComponent<Animator>();
 
-        m_Slider = GameObject.Find("AnimationSpeedSlider").GetComponent<Slider>();
-        m_Slider.onValueChanged.AddListener(delegate {AnimatorSpeed(); });
+        // ANIMATOR CONTROLS
 
         b_Play = GameObject.Find("Button_Play").GetComponent<Button>();
         b_Play.onClick.AddListener(AnimatorPlay);
         
         b_Stop = GameObject.Find("Button_Stop").GetComponent<Button>();
         b_Stop.onClick.AddListener(AnimatorStop);
+
+        b_Reset = GameObject.Find("Button_Reset").GetComponent<Button>();
+        b_Reset.onClick.AddListener(AnimatorReset);
+
+        m_Slider = GameObject.Find("AnimationSpeedSlider").GetComponent<Slider>();
+        m_Slider.onValueChanged.AddListener(delegate {AnimatorSpeed(); });
+
+        // ANIMATOR
+
+        m_Animator = GameObject.Find("SampleCharacter").transform.GetChild(0).gameObject.GetComponent<Animator>();
+
     }
 
     void Update()
@@ -35,17 +47,22 @@ public class AnimatorController : MonoBehaviour
         }
     }
 
-    void AnimatorStop(){
+    void AnimatorReset() {
+
+        Debug.Log("HEHEHE");
+    }
+
+    void AnimatorStop() {
         playing = false;
         m_Animator.speed = 0.0f;
     }
 
-    void AnimatorPlay(){
+    void AnimatorPlay() {
         playing = true;
         m_Animator.speed = speed;
     }
 
-    void AnimatorSpeed(){
+    void AnimatorSpeed() {
         speed = m_Slider.value * 10.0f;
     }
 
