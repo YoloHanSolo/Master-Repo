7bf556dfe3c1beecf9f3256a94ca95094ea03be1
JanPelicon <jnpelicon@gmail.com>
2023-06-28 17:59:30 +0200

    Prev and Next buttons images + Colors on perspective buttons + other fixes

---
+++ Unity/Assets/ControllerAnimator.cs
@@ -0,0 +1,194 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+using TMPro;
+
+
+[System.Serializable]
+public class DictionaryObject
+{
+    public string term;
+    public string definition; 
+}
+
+[System.Serializable]
+public class JsonRoot
+{
+    public DictionaryObject[] dictionary;
+}
+
+
+public class ControllerAnimator : MonoBehaviour
+{
+    bool playing;
+
+    public Sprite sprite_play;
+    public Sprite sprite_pause;
+
+    Button button_playPause;
+    Button button_stop;
+    Button button_next;
+    Button button_prev;
+
+    Slider slider_animation_speed;
+    Slider slider_animation_elapsed;
+
+    TextAsset jsonFile;
+    JsonRoot root;
+
+    TMP_Text term;
+    TMP_Text definition;
+
+    Animator animator;
+    AnimatorStateInfo animator_state_info;
+
+    int current_term_index = 0; 
+
+    private void EventHandler(BaseEventData eventData) {
+        animator.Play(animator_state_info.fullPathHash, 0, slider_animation_elapsed.value);
+        animator.speed = 0.0f;
+
+        playing = false;
+        button_playPause.image.sprite = sprite_play;
+    }
+
+    void Start()
+    {
+        animator = GameObject.Find("Lynn").GetComponent<Animator>();
+        term = GameObject.Find("Word").GetComponent<TextMeshProUGUI>();
+        definition = GameObject.Find("Definition").GetComponent<TextMeshProUGUI>();
+
+        initSliderAnimationElapsed();
+        initSliderAnimationSpeed();
+        initButtons();
+        initDictionary();
+  
+        playing = true;
+    }
+
+    void Update()
+    {
+        animator_state_info = animator.GetCurrentAnimatorStateInfo(0);
+
+        if (playing) {
+            // ANIMATION PLAYING
+            if (animator_state_info.normalizedTime >= 0.0f && animator_state_info.normalizedTime <= 1.0f) {
+                slider_animation_elapsed.value = animator_state_info.normalizedTime;
+            }
+            // ANIMATION OVER
+            if (animator_state_info.normalizedTime > 1.0f) {
+                slider_animation_elapsed.value = 1.0f;
+                animator.Play(animator_state_info.fullPathHash, 0, 0.0f);
+                Animation_Stop_Playing();
+            }
+
+        } 
+    }
+
+    //////////
+    // INIT //
+    //////////
+
+    void initSliderAnimationElapsed() {
+        slider_animation_elapsed = GameObject.Find("Slider_AnimationElapsed").GetComponent<Slider>();
+
+        EventTrigger eventTrigger = GameObject.Find("Slider_AnimationElapsed").AddComponent<EventTrigger>();
+        EventTrigger.Entry clickEvent = new EventTrigger.Entry() {
+            eventID = EventTriggerType.Drag
+        };
+        clickEvent.callback.AddListener(EventHandler);
+        eventTrigger.triggers.Add(clickEvent);
+    }
+
+    void initSliderAnimationSpeed() {
+        slider_animation_speed = GameObject.Find("Slider_AnimationSpeed").GetComponent<Slider>();
+        slider_animation_speed.value = 1.0f;
+        slider_animation_speed.onValueChanged.AddListener(
+            delegate {AnimatorSpeed();}
+        );
+    }
+
+    void initButtons() {
+        button_stop = GameObject.Find("Button_Stop").GetComponent<Button>();
+        button_stop.onClick.AddListener(StopHandler);
+
+        button_playPause = GameObject.Find("Button_PlayPause").GetComponent<Button>();
+        button_playPause.onClick.AddListener(PlayPauseHandler);  
+        button_playPause.image.sprite = sprite_pause;    
+
+        button_next = GameObject.Find("Button_Next").GetComponent<Button>();
+        button_next.onClick.AddListener(Handle_Button_Next);
+
+        button_prev = GameObject.Find("Button_Prev").GetComponent<Button>();
+        button_prev.onClick.AddListener(Handle_Button_Prev);
+    }
+
+    void initDictionary() {
+        jsonFile = Resources.Load<TextAsset>("dictionary");
+        root = JsonUtility.FromJson<JsonRoot>(jsonFile.text);
+        term.SetText(root.dictionary[current_term_index].term);
+        definition.SetText(root.dictionary[current_term_index].definition);
+    }
+
+    //////////////
+    // HANDLERS //
+    //////////////
+
+    void StopHandler() {
+        animator.Play(animator_state_info.fullPathHash, 0, 0.0f);
+        slider_animation_elapsed.value = 0.0f;
+        Animation_Stop_Playing();
+    }
+
+    void PlayPauseHandler() {
+        if (playing) { 
+            Animation_Stop_Playing();
+        } else {
+            if (animator_state_info.normalizedTime > 0.99f) {
+                animator_state_info = animator.GetCurrentAnimatorStateInfo(0);
+                slider_animation_elapsed.value = 0.0f;
+            }
+            Animation_Start_Playing();
+        }
+    }
+
+    void AnimatorSpeed() {
+        if (playing) {
+            animator.speed = slider_animation_speed.value;
+        }
+    }
+
+    void Handle_Button_Next() {
+        current_term_index = (current_term_index + 1) % root.dictionary.Length;
+        term.SetText(root.dictionary[current_term_index].term);
+        definition.SetText(root.dictionary[current_term_index].definition);
+        animator.SetInteger("AnimationCounter", current_term_index);
+        Animation_Start_Playing();
+    }
+
+    void Handle_Button_Prev() {
+        current_term_index = current_term_index - 1;
+        if (current_term_index < 0) {
+            current_term_index = root.dictionary.Length - 1;
+        }     
+        term.SetText(root.dictionary[current_term_index].term);
+        definition.SetText(root.dictionary[current_term_index].definition);
+        animator.SetInteger("AnimationCounter", current_term_index);
+        Animation_Start_Playing();
+    }
+
+    void Animation_Start_Playing() {
+        playing = true;
+        animator.speed = slider_animation_speed.value;
+        button_playPause.image.sprite = sprite_pause;
+    }
+
+    void Animation_Stop_Playing() {
+        playing = false;
+        animator.speed = 0.0f;
+        button_playPause.image.sprite = sprite_play;
+    }
+
+}
--- Unity/Assets/ControllerCamera.cs
+++ Unity/Assets/ControllerCamera.cs
@@ -17,17 +17,20 @@ public class ControllerCamera : MonoBehaviour
     Button button_perspective_hands;
     Button button_perspective_face;
 
+    ColorBlock color_active;
+    ColorBlock color_disabled;
+
     void Start()
     {
         button_perspective_default = GameObject.Find("Button_PerspectiveDefault").GetComponent<Button>();
-        button_perspective_default.onClick.AddListener(HandlerDefault);  
+        button_perspective_default.onClick.AddListener(Handle_Button_Default);  
         button_perspective_default.Select();
 
         button_perspective_hands = GameObject.Find("Button_PerspectiveHands").GetComponent<Button>();
-        button_perspective_hands.onClick.AddListener(HandlerHands);
+        button_perspective_hands.onClick.AddListener(Handle_Button_Hands);
 
         button_perspective_face = GameObject.Find("Button_PerspectiveFace").GetComponent<Button>();
-        button_perspective_face.onClick.AddListener(HandlerFace);
+        button_perspective_face.onClick.AddListener(Handle_Button_Face);
 
         perspectives = new Button[] {
             button_perspective_default,
@@ -42,27 +45,55 @@ public class ControllerCamera : MonoBehaviour
         camera_default.gameObject.SetActive(true);
         camera_hands.gameObject.SetActive(false);
         camera_face.gameObject.SetActive(false);
+
+
+        color_active.disabledColor = new Color(1, 1, 1, 1);
+        color_active.highlightedColor = new Color(1, 1, 1, 1);
+        color_active.normalColor = new Color(1, 1, 1, 1);
+        color_active.pressedColor = new Color(1, 1, 1, 1);
+        color_active.selectedColor = new Color(1, 1, 1, 1);
+        color_active.colorMultiplier = 1.0f;
+
+        color_disabled.disabledColor = new Color(1, 1, 1, 1);
+        color_disabled.highlightedColor = new Color(1, 1, 1, 1);
+        color_disabled.normalColor = new Color(0.5f, 0.5f, 0.5f, 1);
+        color_disabled.pressedColor = new Color(1, 1, 1, 1);
+        color_disabled.selectedColor = new Color(1, 1, 1, 1);
+        color_disabled.colorMultiplier = 1.0f;
+
+        button_perspective_hands.colors = color_disabled;
+        button_perspective_face.colors = color_disabled;
     }
 
     void Update() {
 
     }
 
-    void HandlerDefault() {
+    void Handle_Button_Default() {
         if (button_select_index != 0) {
+
             button_select_index = 0;
             perspectives[button_select_index].Select();
-            
+
+            button_perspective_default.colors = color_active;
+            button_perspective_hands.colors = color_disabled;
+            button_perspective_face.colors = color_disabled;
+
             camera_default.gameObject.SetActive(true);
             camera_hands.gameObject.SetActive(false);
             camera_face.gameObject.SetActive(false);
         }
     }
 
-    void HandlerHands() {
+    void Handle_Button_Hands() {
         if (button_select_index != 1) {
+
             button_select_index = 1;
             perspectives[button_select_index].Select();
+
+            button_perspective_default.colors = color_disabled;
+            button_perspective_hands.colors = color_active;
+            button_perspective_face.colors = color_disabled;
 
             camera_default.gameObject.SetActive(false);
             camera_hands.gameObject.SetActive(true);
@@ -70,10 +101,15 @@ public class ControllerCamera : MonoBehaviour
         }
     }
 
-    void HandlerFace() {
+    void Handle_Button_Face() {
         if (button_select_index != 2) {
+
             button_select_index = 2;
             perspectives[button_select_index].Select();
+
+            button_perspective_default.colors = color_disabled;
+            button_perspective_hands.colors = color_disabled;
+            button_perspective_face.colors = color_active;
 
             camera_default.gameObject.SetActive(false);
             camera_hands.gameObject.SetActive(false);
