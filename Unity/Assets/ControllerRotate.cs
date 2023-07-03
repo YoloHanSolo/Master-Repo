cfd4673e45a476d0576453a55d83df8798680797
JanPelicon <jnpelicon@gmail.com>
2023-07-03 16:29:09 +0200

    Improved rotation

--- Unity/Assets/ControllerRotate.cs
+++ Unity/Assets/ControllerRotate.cs
@@ -11,7 +11,13 @@ public class ControllerRotate : MonoBehaviour, IPointerEnterHandler, IPointerExi
     public CursorMode cursorMode = CursorMode.Auto;
     public Vector2 hotSpot = Vector2.zero;
 
+    public float rotation_sensitivity = 50.0f;
+
     GameObject lynn;
+
+    Vector3 mouse_now;
+    Vector3 mouse_prev;
+    float mouse_delta_x;
 
     bool mouse_over = false;
 
@@ -20,15 +26,19 @@ public class ControllerRotate : MonoBehaviour, IPointerEnterHandler, IPointerExi
         lynn = GameObject.Find("Lynn");
     }
 
-    void Update() {        
+    void Update() {
+        mouse_now = Input.mousePosition;
+        if (mouse_prev != null) {
+            mouse_delta_x = mouse_now.x - mouse_prev.x;
+        }
+        mouse_prev = Input.mousePosition;
+
         if (mouse_over) {
             if (Input.GetMouseButton(0)) {
-                float new_ry = lynn.transform.rotation.y + 1;
-                if (new_ry > 360) {
-                    new_ry = 0;
-                }
-
-                lynn.transform.Rotate(0, new_ry, 0);
+                lynn.transform.eulerAngles += new Vector3(0, -mouse_delta_x * rotation_sensitivity * Time.deltaTime, 0);
+            }
+            if (Input.GetMouseButton(1)) {
+                lynn.transform.eulerAngles = new Vector3(0, 180, 0);
             }
         }
 
